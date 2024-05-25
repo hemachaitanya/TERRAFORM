eadec8479fcbb041cb6408965978d1cb6027b23a
hemachaitanya <123231061+hemachaitanya@users.noreply.github.com>
2024-05-25 14:46:03 +0530

    Update countfunction.tf

--- countfunction.tf
+++ countfunction.tf
@@ -18,3 +18,24 @@ resource "aws_subnet" "awssub" {
 
 }
 
+
+### length function
+# Create subnets
+variable "subnet_cidr_blocks" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+}
+
+resource "aws_subnet" "my_subnets" {
+  count           = length(var.subnet_cidr_blocks)
+  vpc_id          = aws_vpc.my_vpc.id
+  cidr_block      = var.subnet_cidr_blocks[count.index]
+  availability_zone = "your_availability_zone" # Specify the availability zone here
+}
+
